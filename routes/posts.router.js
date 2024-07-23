5e46c1bf98ea178c4ffb592c43ffd4cf69d3e063
irineonyx <85147915+irineonyx@users.noreply.github.com>
2024-07-23 19:38:00 +0700

    add header before route

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 const postsController = require("../controller/posts.controller")
 
+express.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
+  
+    next();
+  });
 router.get("/", postsController.getAll)
 router.get("/:id", postsController.getById)
 router.post("/", postsController.create)
